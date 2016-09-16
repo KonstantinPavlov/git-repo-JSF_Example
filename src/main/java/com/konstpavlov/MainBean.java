d6ca3c84e626ccc508cd884c38e033e7120b2369
Konstantin <konstpavlovsdr@gmail.com>
2016-09-16 23:21:10 +0300

    Initial commit

---
+++ src/main/java/com/konstpavlov/MainBean.java
@@ -0,0 +1,38 @@
+package com.konstpavlov;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+/**
+ * Created by Konstantin on 16.09.2016.
+ *
+ *
+ *
+ */
+
+@ManagedBean(name = "mainBean")
+@SessionScoped
+public class MainBean {
+
+    private String inputText;
+
+    public String getInputText() {
+        return inputText;
+    }
+
+    public void setInputText(String inputText) {
+        this.inputText = inputText;
+    }
+
+    public void showMessage()
+    {
+        FacesMessage message=new FacesMessage("Заголовок","Частичное обновление страницы");
+        message.setSeverity(FacesMessage.SEVERITY_INFO);
+        FacesContext.getCurrentInstance().addMessage(null,message);
+
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,"Всплываюее окно","GrowlMessage"));
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_FATAL,"Значение : ",inputText));
+    }
+}
